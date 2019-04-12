e15c3868326d5fe77f3e33945635513125682e67
govindarajs <govindarajs@dckap.com>
2019-04-12 16:36:56 +0530

    Updates

---
+++ precache-manifest.f88bf3a0c12cf2fcd0276da16d0ba4f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a86efb9098f7f12161f",
+    "url": "/govind-drinkWater/static/css/main.c2571382.chunk.css"
+  },
+  {
+    "revision": "9a86efb9098f7f12161f",
+    "url": "/govind-drinkWater/static/js/main.f7389755.chunk.js"
+  },
+  {
+    "revision": "f1fc37f492c7ff717493",
+    "url": "/govind-drinkWater/static/js/runtime~main.cc1ba082.js"
+  },
+  {
+    "revision": "6d999cfb3d88da4206a4",
+    "url": "/govind-drinkWater/static/css/2.07b8b5d8.chunk.css"
+  },
+  {
+    "revision": "6d999cfb3d88da4206a4",
+    "url": "/govind-drinkWater/static/js/2.12338a1a.chunk.js"
+  },
+  {
+    "revision": "51d2f40bfc8408b2ca2bef07ca376592",
+    "url": "/govind-drinkWater/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/myapp/precache-manifest.d1e12ca679cd1f71a3b4d180c230c452.js"
+  "/govind-drinkWater/precache-manifest.f88bf3a0c12cf2fcd0276da16d0ba4f3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/myapp/index.html", {
+workbox.routing.registerNavigationRoute("/govind-drinkWater/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
