de428fe5a9af74a97078dea482f2d6018394e000
govindarajs <govindarajs@dckap.com>
2019-04-12 16:35:33 +0530

    Updates

---
+++ precache-manifest.d1e12ca679cd1f71a3b4d180c230c452.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed464c38ed8185f7a94b",
+    "url": "/myapp/static/css/main.c2571382.chunk.css"
+  },
+  {
+    "revision": "ed464c38ed8185f7a94b",
+    "url": "/myapp/static/js/main.d42df7c4.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "6d999cfb3d88da4206a4",
+    "url": "/myapp/static/css/2.07b8b5d8.chunk.css"
+  },
+  {
+    "revision": "6d999cfb3d88da4206a4",
+    "url": "/myapp/static/js/2.12338a1a.chunk.js"
+  },
+  {
+    "revision": "a78ae992f51b419a44dec3e76abfaf70",
+    "url": "/myapp/index.html"
+  }
+];
